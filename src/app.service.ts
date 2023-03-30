291237e78cb8f613e9f1c1ed552acae441c67060
Ngoc Nguyen <duyngoc.nguyen2001@gmail.com>
2023-03-30 09:28:55 +0700

    get links api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,38 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getLinks() {
+    return [
+      {
+        name: 'XOILAC',
+        url: 'https://xem9.xoilac12.org/?utm_source=landing&utm_medium=landing',
+        logo: 'https://90m.link/images/logo-xoilac.png',
+      },
+      {
+        name: 'VEBOtv',
+        url: 'https://tructiep.vebo3.org/?utm_source=landing&utm_medium=landing',
+        logo: 'https://90m.link/images/logo-01.png',
+      },
+      {
+        name: 'BanhKhuc',
+        url: 'https://xem9.banhkhuc6.com/?utm_source=landing&utm_medium=landing',
+        logo: 'https://90m.link/images/logo-bk.png',
+      },
+      {
+        name: '90Phut',
+        url: 'https://tructiep.90phut4.live/?utm_source=landing&utm_medium=landing',
+        logo: 'https://90m.link/images/logo-90.png',
+      },
+      {
+        name: 'Group Facebook',
+        url: 'https://www.facebook.com/groups/3069898683235513/',
+        logo: 'https://90m.link/images/logo-facebook.png',
+      },
+      {
+        name: 'Không Sợ Bóng',
+        url: 'https://www.facebook.com/groups/3069898683235513/',
+        logo: 'https://90m.link/images/logo-youtube.png',
+      },
+    ];
   }
 }
