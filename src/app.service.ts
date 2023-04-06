8b689f257ac98eb6a39823f9b467acfab211f6b7
Ngoc Nguyen <duyngoc.nguyen2001@gmail.com>
2023-04-06 17:34:12 +0700

    update links

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,22 +6,22 @@ export class AppService {
     return [
       {
         name: 'XOILAC',
-        url: 'https://xem9.xoilac12.org/?utm_source=landing&utm_medium=landing',
+        url: 'https://tructiep.xoilac12.org/?utm_source=landing&utm_medium=landing',
         logo: '/logo-xoilac.png',
       },
       {
         name: 'VEBOtv',
-        url: 'https://tructiep.vebo3.org/?utm_source=landing&utm_medium=landing',
+        url: 'https://tructiep2.vebo3.org/?utm_source=landing&utm_medium=landing',
         logo: '/logo-01.png',
       },
       {
         name: 'BanhKhuc',
-        url: 'https://xem9.banhkhuc6.com/?utm_source=landing&utm_medium=landing',
+        url: 'https://tructiep.banhkhuc6.com/?utm_source=landing&utm_medium=landing',
         logo: '/logo-bk.png',
       },
       {
         name: '90Phut',
-        url: 'https://tructiep.90phut4.live/?utm_source=landing&utm_medium=landing',
+        url: 'https://tructiep2.90phut4.live/?utm_source=landing&utm_medium=landing',
         logo: '/logo-90.png',
       },
       {
